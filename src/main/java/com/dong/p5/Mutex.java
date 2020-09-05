3272b096c004b791e867cea62b189bef37688771
Mulavar <dongjianhui@hyperchain.cn>
2020-09-05 18:47:59 +0800

    增加自定义锁示例

---
+++ src/main/java/com/dong/p5/Mutex.java
@@ -0,0 +1,87 @@
+package com.dong.p5;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * 5.2.1 队列同步器示例
+ * <p>
+ * Lock面向使用者提供接口
+ * AQS面向实现者提供接口
+ *
+ * @author Lam
+ * @date 2020/7/27
+ */
+public class Mutex implements Lock {
+    private final Sync sync = new Sync();
+
+    /**
+     * 同步器实现，重写非模板方法
+     * 主要思想是使用CAS设置state的状态
+     */
+    private static class Sync extends AbstractQueuedSynchronizer {
+        @Override
+        protected boolean tryAcquire(int arg) {
+            if (compareAndSetState(0, 1)) {
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        protected boolean tryRelease(int arg) {
+            if (getState()==0) {
+                throw new IllegalMonitorStateException();
+            }
+            setExclusiveOwnerThread(null);
+            setState(0);
+            return true;
+        }
+
+        @Override
+        protected boolean isHeldExclusively() {
+            return getState() == 1;
+        }
+
+        Condition newCondition() {
+            return new ConditionObject();
+        }
+    }
+
+    /**
+     * Lock接口的实现
+     */
+
+    @Override
+    public void lock() {
+        sync.acquire(1);
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+
+    @Override
+    public boolean tryLock() {
+        return sync.tryAcquire(1);
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return sync.tryAcquireNanos(1, unit.toNanos(time));
+    }
+
+    @Override
+    public void unlock() {
+        sync.release(1);
+    }
+
+    @Override
+    public Condition newCondition() {
+        return sync.newCondition();
+    }
+}
